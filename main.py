179f9b8b97639ecd145ecd66c414a3725608dbe1
KadenSchumacher10 <825371@mystma.org>
2022-09-16 09:15:04 -0500

    1

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def topFiveMovies(a, b, c,  d, e):
-    return(a + b + c + d + e)
-print(topFiveMovies("5. despicable me " , "4. despicable me 2 " , "3. despicable me 3 " , "2. minions rise of gru " , "1. minions "))
+def topFiveMovies(a, b, c, d, e):
+    return(a + " " + b + " " + c + " " + d + " " + e + " ")
+print(topFiveMovies("5. despicable me" , "4. despicable me 2" , "3. despicable me 3" , "2. minions rise of gru" , "1. minions"))
